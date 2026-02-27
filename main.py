bd4f813ec561535e3e21c8c071e37964993cf923
cxbrillant-ctrl <cxbrillant@students.everettcc.edu>
2026-02-27 04:50:58 +0000

    complete

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print ("""*** Welcome to our online car dealer ship ***
+   * we open from monday to saturday *
+       * Time : 7h AM to  5h PM *\n
+    """)
+
+print( "Here is the avaible list of car we have" )
